41ba7dd1ed5dde38d9f8e85dd027179ba58d36b0
Jukka Svahn <web@rahinaa.biz>
2013-08-10 01:50:47 +0300

    Comments.

--- src/Arc/Twitter/Admin.php
+++ src/Arc/Twitter/Admin.php
@@ -6,12 +6,20 @@
 
 class Arc_Twitter_Admin
 {
+    /**
+     * Constructor.
+     */
+
     public function __construct()
     {
         add_privs('arc_twitter', '1,2,3,4');
         register_tab('extensions', 'arc_twitter', gTxt('arc_twitter'));
         register_callback(array($this, 'pane'), 'arc_twitter');
     }
+
+    /**
+     * The pane.
+     */
 
     public function pane()
     {
