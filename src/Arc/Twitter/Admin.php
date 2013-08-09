9d8b55fd45cefa81e65b76fd51b812c39a82beca
Jukka Svahn <web@rahinaa.biz>
2013-08-10 01:00:48 +0300

    Proper event name.

--- src/Arc/Twitter/Admin.php
+++ src/Arc/Twitter/Admin.php
@@ -8,7 +8,7 @@ class Arc_Twitter_Admin
 {
     public function __construct()
     {
-        add_privs('arc_admin_twitter', '1,2,3,4');
+        add_privs('arc_twitter', '1,2,3,4');
         register_tab('extensions', 'arc_twitter', gTxt('arc_twitter'));
         register_callback(array($this, 'pane'), 'arc_twitter');
     }
