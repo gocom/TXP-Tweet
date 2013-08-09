bacc530c4d0d24d9e70647624f9a632725683d10
Jukka Svahn <web@rahinaa.biz>
2013-08-10 00:59:10 +0300

    PSR-0 file structure.

---
+++ src/Arc/Twitter/Install.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Installer.
+ */
+
+class Arc_Twitter_Install
+{
+    /**
+     * An array of plugin preference strings.
+     *
+     * @var array
+     */
+
+    protected $prefs = array(
+        'user'                => array('text_input', ''),
+        'prefix'              => array('text_input', 'Just posted:'),
+        'suffix'              => array('text_input', ''),
+        'cache_dir'           => array('text_input', ''), // TODO: remove?
+        'tweet_default'       => array('yesnoradio', 1),
+        'url_method'          => array('arc_twitter_url_method_select', 'tinyurl'),
+        'access_token'        => array('text_input', ''),
+        'access_token_secret' => array('text_input', ''),
+        'consumerkey'         => array('text_input', ''),
+        'consumersecret'      => array('text_input', ''),
+    );
+
+    /**
+     * Constructor.
+     */
+
+    public function __construct()
+    {
+        add_privs('prefs.arc_twitter', '1');
+        register_callback(array($this, 'install'), 'plugin_lifecycle.arc_twitter', 'installed');
+        register_callback(array($this, 'uninstall'), 'plugin_lifecycle.arc_twitter', 'deleted');
+    }
+
+    /**
+     * Installer.
+     */
+
+    public function install()
+    {
+        safe_query(
+            "CREATE TABLE IF NOT EXISTS ".safe_pfx('arc_twitter')." (
+                id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+                article INTEGER(11) NOT NULL default 0,
+                tweet_id VARCHAR(255) NOT NULL default '',
+                status VARCHAR(140) NOT NULL default '',
+                url VARCHAR(255)
+            ) PACK_KEYS=1 AUTO_INCREMENT=1 CHARSET=utf8"
+        );
+
+        $position = 1;
+
+        foreach ($this->prefs as $name => $pref)
+        {
+            if (($name = 'arc_twitter_' . $name) && get_pref($name, false) === false)
+            {
+                set_pref($name, $pref[1], 'arc_twitter', $position, $pref[0]);
+            }
+
+            $position++;
+        }
+    }
+
+    /**
+     * Uninstaller.
+     */
+
+    public function uninstall()
+    {
+        safe_query("DROP TABLE IF EXISTS ".safe_pfx('arc_twitter'));
+        safe_delete('txp_prefs', "event = 'arc_twitter'");
+    }
+}
---
+++ src/Arc/Twitter/Publish.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Auto-publish tweets.
+ */
+
+class Arc_Twitter_Publish
+{
+    /**
+     * Constructor.
+     */
+
+    public function __construct()
+    {
+        register_callback(array($this, 'tweet'), 'article_saved');
+        register_callback(array($this, 'tweet'), 'article_posted');
+        register_callback(array($this, 'ui'), 'article_ui', 'status');
+    }
+
+    /**
+     * Publishes the article on Twitter.
+     *
+     * @todo Shorten the URL
+     */
+
+    public function tweet($event, $step, $r)
+    {
+        extract(psa(array(
+            'arc_twitter_prefix',
+            'arc_twitter_suffix',
+            'arc_twitter_tweet',
+        )));
+
+        // Title is escaped for SQL in 4.5.x. Due to this bug, we need to pull it from the DB.
+
+        if (!$arc_twitter_tweet || !($url = permlinkurl_id($r['ID'])) || !($title = trim(safe_field('Title', 'textpattern', 'ID = '.intval($r['ID']).' and Status = '.STATUS_LIVE))))
+        {
+            return;
+        }
+
+        if (safe_row('arc_twitter', "article = ".intval($r['ID'])))
+        {
+            return;
+        }
+
+        $status = array(
+            $arc_twitter_prefix,
+            $title,
+            $url,
+            $arc_twitter_suffix,
+        );
+
+        if (($over = strlen(trim(join(' ', $status))) - 140) && $over > 0)
+        {
+            $status[1] = substr($status[1], 0, $over * -1);
+        }
+
+        $status = trim(join(' ', $status));
+        $twitter = new Arc_Twitter_API();
+        $result = $twitter->post('statuses/update', array('status' => $status));
+
+        if ($result && $result->id)
+        {
+            safe_insert(
+                'arc_twitter',
+                "article = ".intval($r['ID']).",
+                tweet_id = '".doSlash($tweet_id)."',
+                status = '".doSlash($status)."',
+                url = '".doSlash($url)."'"
+            );
+        }
+    }
+
+    /**
+     * Tweet options group on Write panel.
+     */
+
+    public function ui($event, $step, $default, $rs)
+    {
+        return $default . wrapRegion(
+            'arc_twitter_tweet',
+
+            graf(
+                checkbox('arc_twitter_tweet', 1, '', '', 'reset_time').
+                tag(gTxt('arc_twitter_tweet_this'), 'label', array('for' => 'reset_time'))
+            ).
+
+            graf(
+                tag(gTxt('arc_twitter_prefix'), 'label', array('for' => 'arc_twitter_prefix')).br.
+                fInput('text', 'arc_twitter_prefix', '', '', '', '', INPUT_REGULAR, '', 'arc_twitter_prefix')
+            ).
+
+            graf(
+                tag(gTxt('arc_twitter_suffix'), 'label', array('for' => 'arc_twitter_suffix')).br.
+                fInput('text', 'arc_twitter_suffix', '', '', '', '', INPUT_REGULAR, '', 'arc_twitter_suffix')
+            ),
+
+            '',
+
+            gTxt('arc_twitter')
+        );
+    }
+}
+
+new Arc_Twitter_Publish();
